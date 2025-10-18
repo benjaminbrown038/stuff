4063fccf70c80d54c048a0ad48357daaaf00f903
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2025-10-17 21:16:50 -0400

    Add object detection methods to getting-started.py

---
+++ Detection/getting-started.py
@@ -0,0 +1,12 @@
+Object Detection
+Two Stage Detectors
+R CNN
+Fast R CNN
+Faster R CNN
+Single Stage Detectors
+YOLO
+Single Shot MultiBox Detector
+RetinaNet
+Transformer Based Detectors
+DETR
+Deformable DETR
