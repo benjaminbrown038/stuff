5c04bb6e915d6c2c8d76a04d5cf57b3674ab03af
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2025-10-18 01:11:32 -0400

    Create live_inference.py with initial comments
    Add initial comments and structure for live inference script

---
+++ Detection/live_inference.py
@@ -0,0 +1,9 @@
+# utilities: build/load model + predictor 
+
+# frame - predictions - overlay
+
+# live webcam (desktop / local pyton)
+
+# video file - output mp4 (colab friendly)
+
+# optional: save / load your fine-tuned weights
