768229b3996a8cf54d392b518a4dc9faab87df1d
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2025-10-18 01:07:44 -0400

    Add segmentation pipeline skeleton code

---
+++ Detection/segmentation-pipeline.py
@@ -0,0 +1,21 @@
+# setup 
+
+# imports and configurations
+
+# dataset
+
+# dataset class 
+
+# dataloaders
+
+# model setup
+
+# optimizer 
+
+# training loop + mAP / IoU Tracking
+
+# IoU Plot 
+
+# Inference and Visualization
+
+# Evaluation With COCO Style mAP
